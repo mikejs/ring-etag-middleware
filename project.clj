31cd60d3529fcd320ffe724b0b1f890b62087ae0
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2012-11-01 23:13:24 -0400

    project.clj: bump version to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject org.clojars.mikejs/ring-etag-middleware "0.1.0-SNAPSHOT"
+(defproject org.clojars.mikejs/ring-etag-middleware "0.1.1-SNAPSHOT"
   :descriptions "Ring etag middleware"
   :dependencies [[org.clojure/clojure "1.4.0"]])
